760aa7aa6e3ca577392a488139dbe82a2da94a4c
asmoorr <90kidex90@gmail.com>
2024-11-07 10:15:49 +0300

    Created first version of all models

--- BackEnd/medchainapi/admin.py
+++ BackEnd/medchainapi/admin.py
@@ -1,1 +1,8 @@
-# Register your models here.
+from django.contrib import admin
+from .models import Patient, Doctor, MedicalInstitution, AccessRequest, ActionLog
+
+admin.site.register(Patient)
+admin.site.register(Doctor)
+admin.site.register(MedicalInstitution)
+admin.site.register(AccessRequest)
+admin.site.register(ActionLog)
