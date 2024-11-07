d4c3a9ed23b0f40c2a6034358734fecfd83d55e9
asmoorr <90kidex90@gmail.com>
2024-11-07 14:06:05 +0300

    Added models to admin panel

--- BackEnd/medchainapi/admin.py
+++ BackEnd/medchainapi/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Patient, Doctor, MedicalInstitution, AccessRequest, ActionLog
+from .models import Patient, Doctor, MedicalOrganization, AccessRequest, ActionLog
 
 admin.site.register(Patient)
 admin.site.register(Doctor)
-admin.site.register(MedicalInstitution)
+admin.site.register(MedicalOrganization)
 admin.site.register(AccessRequest)
 admin.site.register(ActionLog)
