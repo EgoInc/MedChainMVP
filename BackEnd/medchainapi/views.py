50a54fe64130e24fe86352e84f0e969e9ca63869
asmoorr <90kidex90@gmail.com>
2024-11-07 14:10:57 +0300

    Added TODO and renamed patient serializer

--- BackEnd/medchainapi/views.py
+++ BackEnd/medchainapi/views.py
@@ -3,22 +3,22 @@ from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import PatientDataSerializer
+from .serializers import PatientSerializer
 
 
 class PatientDataView(APIView):
     @extend_schema(
         summary="Получение данных пациента",
         description="Позволяет врачу получить базовые данные о пациенте для подтверждения его личности.",
-        responses={status.HTTP_200_OK: PatientDataSerializer},
+        responses={status.HTTP_200_OK: PatientSerializer},
     )
     def get(self, request, doctor_id, patient_id):
-        # Заглушка данных, замените на запрос к базе данных, если необходимо
+        # Заглушка данных TODO: сделать обращение к базе
         patient_data = {
             "patient_id": patient_id,
             "name": "Иванов Иван Иванович",
             "date_of_birth": "2000-06-01",
             "contract_address": "0x0000000000000000000000000000000000000000",
         }
-        serializer = PatientDataSerializer(patient_data)
+        serializer = PatientSerializer(patient_data)
         return Response(serializer.data, status=status.HTTP_200_OK)
