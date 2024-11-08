1f56b8d38ce934827f8ebee92f059be14774dc11
asmoorr <90kidex90@gmail.com>
2024-11-09 02:33:36 +0300

    Added urls for doctors requests

--- BackEnd/medchainapi/urls.py
+++ BackEnd/medchainapi/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import PatientDataView
+
+from .views import PatientDataView, AccessRequestView, PatientSearchView, DoctorPatientListView
 
 urlpatterns = [
     path('doctor/<int:doctor_id>/patient/<int:patient_id>/', PatientDataView.as_view(), name='patient-data'),
+    path('doctor/<int:doctor_id>/request-access', AccessRequestView.as_view(), name='request-access'),
+    path('doctor/<int:doctor_id>/search-patients', PatientSearchView.as_view(), name='search-patients'),
+    path('doctor/<int:doctor_id>/my-patients', DoctorPatientListView.as_view(), name='patients_of_doctor')
 ]
