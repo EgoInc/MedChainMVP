c897ffe16284e75c848508fc04e4c69b23b68c9e
emopudge <emiliakudelenskaa@gmail.com>
2024-11-12 01:23:30 +0300

    started fixing patient requests

--- BackEnd/medchainapi/views.py
+++ BackEnd/medchainapi/views.py
@@ -119,10 +119,15 @@ class AddPatientView(APIView):
         responses={status.HTTP_201_CREATED: AddPatientSerializer,
                    status.HTTP_400_BAD_REQUEST: 'Неверные данные'},
     )
-    def post(self, request):
-        serializer = AddPatientSerializer(data=request.data)
+    def post(self, request, name, date_of_birth, contract_adress):
+        request_data = {
+            "name": "Полное имя пациента. Тип: string",
+            "date_of_birth": "Дата рождения пациента. Тип: string (ISO 8601)",
+            "contract_address": "Адрес смарт-контракта пациента в блокчейне. Тип: string"
+        }
+        serializer = AddPatientSerializer(data=request_data)
+        # TODO: реализовать логику добавления пациента
         if serializer.is_valid():
-            # TODO: реализовать логику добавления пациента
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
