4a399c4f313f2398d2de04651c476a4422ff6a3c
asmoorr <90kidex90@gmail.com>
2024-11-20 00:36:16 +0300

    Add multistage building for docker and proper db connection

--- BackEnd/medchain/settings.py
+++ BackEnd/medchain/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -89,10 +90,16 @@ WSGI_APPLICATION = 'medchain.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+import os
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'medchain_db'),
+        'USER': os.getenv('POSTGRES_USER', 'user'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'password'),
+        'HOST': 'db',  # Название сервиса в docker-compose
+        'PORT': '5432',  # Стандартный порт PostgreSQL
     }
 }
 
