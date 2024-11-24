f74181b98b14c1ae59667a472881752e5466af07
nistya <apple.for.ledi@icloud.com>
2024-11-25 01:35:33 +0300

    fixed routes

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "../src/pages/index.css"; //тут шрифт для всего сделан, плюс сброс margin и padding
-// import App from '../src/pages/App/App'; //тут логика переходов
-import App from "./App";
+import App from "../src/pages/App/App"; //тут логика переходов
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
