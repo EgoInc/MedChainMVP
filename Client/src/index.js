10b3113af494da0591681a7d49babd90e5baab58
bareckij <popajjlox@gmail.com>
2024-11-25 22:49:33 +0300

    changed routes

--- Client/src/index.js
+++ Client/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom";
 import "../src/pages/index.css"; //тут шрифт для всего сделан, плюс сброс margin и padding
-import App from "./pages/LoginRegistration/App"; //тут логика переходов для логина и регистрации
+import App from "./App";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
